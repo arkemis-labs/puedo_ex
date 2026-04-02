337441f86418ac6707cfb547ab785917a3efe1d1
Erik <e.ferrari@arkemis.it>
2026-04-02 17:44:20 +0200

    fix(puedo_ecto): workspace and versioning info

--- packages/puedo/mix.exs
+++ packages/puedo/mix.exs
@@ -7,7 +7,20 @@ defmodule Puedo.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      workspace: [
+        tags: [{:scope, :package}]
+      ],
+      versioning: versioning()
+    ]
+  end
+
+  defp versioning do
+    [
+      tag_prefix: "puedo_ecto@v",
+      commit_msg: "puedo_ecto@v%s",
+      annotation: "tag release-%s",
+      annotate: true
     ]
   end
 
