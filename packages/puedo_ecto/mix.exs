f70ee57ca3460832f56fc062acdb2de5db9372ee
Erik <e.ferrari@arkemis.it>
2026-04-15 14:45:06 +0200

    puedo_ecto@v0.0.1

--- packages/puedo_ecto/mix.exs
+++ packages/puedo_ecto/mix.exs
@@ -4,7 +4,7 @@ defmodule PuedoEcto.MixProject do
   def project do
     [
       app: :puedo_ecto,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
